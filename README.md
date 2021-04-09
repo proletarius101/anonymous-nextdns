bec44f74e87caa2f20c473d25f04c082c21fce54
proletarius101 <proletarius101@protonmail.com>
2021-04-09 16:24:14 +0800

    v0.1.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# Anonymous NextDNS
+
+Enjoy NextDNS's power without registration, because we don't want to link our email addresses.
+
+## Development
+
+``` sh
+$ yarn dev
+```
+
+## Building
+
+```sh
+$ yarn build
+```
+
+## Lint
+
+``` sh
+$ yarn lint
+```
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+VM.observe(document.body, () => {
+    // Find the target node
+    const modalTitle = document.querySelector('.modal-title');
+
+    if (modalTitle.textContent === "Expired") {
+        console.log("Observed!!!")
+        // Remove modal
+        const modal = document.querySelector(".modal")
+        modal.remove()
+        const modalBackdrop = document.querySelector(".modal-backdrop")
+        modalBackdrop.remove()
+
+        // Remove hiding
+        const root = document.querySelector("#root")
+        root.setAttribute("aria-hidden", 'false');
+        const body = document.querySelector("body");
+        body.classList.remove("modal-open")
+        body.style.overflow = "visible"
+
+        // disconnect observer
+        return true;
+    }
+})
